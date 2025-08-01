dbc34d970f6d7d42444f91548cdb540af0f06a51
wigglywoggly <lolw04938@gmail.com>
2025-08-01 19:42:51 +0800

    Update Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -1,138 +1,158 @@
 public class Pokemon {
-   private String name;
-   private int hp;
-   private int attackPower;
-   private String moveType;
-   private String defenderType;
-   private int speed;
-   private int increaseAttack = 0;
-   private int increaseSpeed = 0;
-   private int baseSpeed;
-   private int baseAttackPower;
+	//attributes
+    private String name;
+    private int hp;
+    private int attackPower;
+    private String moveType;
+    private String defenderType;
+    private int speed;
+    private int increaseAttack = 0; // boosted attack power
+    private int increaseSpeed = 0; // boosted speed 
+    private int baseSpeed; // original speed 
+    private int baseAttackPower; // original attack power
+    
 
-   public Pokemon(String name, int hp, int attackPower, int speed, String moveType, String defenderType) {
-      this.name = name;
-      this.hp = hp;
-      this.attackPower = attackPower;
-      this.moveType = moveType;
-      this.defenderType = defenderType;
-      this.speed = speed;
-      this.baseSpeed = speed;
-      this.baseAttackPower = attackPower;
-   }
+    // Constructor
+    public Pokemon(String name, int hp, int attackPower,int speed, String moveType, String defenderType) {
+        this.name = name;
+        this.hp = hp;
+        this.attackPower = attackPower;
+        this.moveType = moveType;
+        this.defenderType = defenderType;
+        this.speed = speed;
+        this.baseSpeed = speed;
+        this.baseAttackPower = attackPower;
+        }
 
-   public String getName() {
-      return this.name;
-   }
+    // Setters Getters
+    
+    //get and set user name
+    public String getName() {
+    	return name;
+    }
+    public void setName(String name) { 
+    	this.name = name; 
+    }
+    
+    //get and set hp
+    public int getHp() { 
+    	return hp; 
+    }
+    
+    public void setHp(int hp) {
+    	this.hp = hp;
+    }
+    
+    public int getSpeed() {
+		return speed;
+	}
 
-   public void setName(String name) {
-      this.name = name;
-   }
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
 
-   public int getHp() {
-      return this.hp;
-   }
+	//get set attack power
+    public int getAttackPower() { 
+    	return attackPower; 
+    }
+    
+    public void setAttackPower(int attackPower) { 
+    	this.attackPower = attackPower;
+    }
+    
+    //get set move type
+    public String getMoveType() { 
+    	return moveType;
+    }
+    public void setMoveType(String moveType) { 
+    	this.moveType = moveType; 
+    }
+    
+    //get set defender type
+    public String getDefenderType() { 
+    	return defenderType; 
+    }
 
-   public void setHp(int hp) {
-      this.hp = hp;
-   }
+    public void setDefenderType(String defenderType) { 
+    	this.defenderType = defenderType;
+    }
+    
+    public int getBaseSpeed() {
+		return baseSpeed;
+	}
 
-   public int getSpeed() {
-      return this.speed;
-   }
+	public int getBaseAttackPower() {
+		return baseAttackPower;
+	}
 
-   public void setSpeed(int speed) {
-      this.speed = speed;
-   }
+	public void setBaseAttackPower(int baseAttackPower) {
+		this.baseAttackPower = baseAttackPower;
+	}
 
-   public int getAttackPower() {
-      return this.attackPower;
-   }
+	// Methods
+    public void reduceHp(int damage) {
+        this.hp = Math.max(0, this.hp - damage);
+        //example = when HP is 10 and damage is 15, (0, 10 - 15) = 0
+    }
 
-   public void setAttackPower(int attackPower) {
-      this.attackPower = attackPower;
-   }
+    public boolean isDefeated() { //checks if pokemon is dead 
+        return this.hp <= 0;
+        //if pokemon's hp = 0, return true. otherwise, bro's still alive
+    }
 
-   public String getMoveType() {
-      return this.moveType;
-   }
-
-   public void setMoveType(String moveType) {
-      this.moveType = moveType;
-   }
-
-   public String getDefenderType() {
-      return this.defenderType;
-   }
-
-   public void setDefenderType(String defenderType) {
-      this.defenderType = defenderType;
-   }
-
-   public int getBaseSpeed() {
-      return this.baseSpeed;
-   }
-
-   public int getBaseAttackPower() {
-      return this.baseAttackPower;
-   }
-
-   public void setBaseAttackPower(int baseAttackPower) {
-      this.baseAttackPower = baseAttackPower;
-   }
-
-   public void reduceHp(int damage) {
-      this.hp = Math.max(0, this.hp - damage);
-   }
-
-   public boolean isDefeated() {
-      return this.hp <= 0;
-   }
-
-   public int attack(Pokemon target) {
-      double effectiveness = TypeEffectiveness.getEffectiveness(this.moveType, target.getDefenderType());
-      int damage = (int)((double)this.attackPower * effectiveness);
-      String var10001 = this.name;
-      System.out.println(var10001 + " attacks " + target.getName() + " for " + damage + " damage!");
-      return damage;
-   }
-
-   public void applyAttackBoost(int boostAmount) {
-      this.increaseAttack += boostAmount;
-      this.attackPower = this.baseAttackPower + this.increaseAttack;
-   }
-
-   public void applySpeedBoost(int boostAmount) {
-      this.increaseSpeed += boostAmount;
-      this.speed = this.baseSpeed + this.increaseSpeed;
-   }
-
-   public void removeSpeedBoost(int boostAmount) {
-      this.increaseSpeed -= boostAmount;
-      if (this.increaseSpeed < 0) {
-         this.increaseSpeed = 0;
-      }
-
-      this.speed = this.baseSpeed + this.increaseSpeed;
-   }
-
-   public void removeAttackBoost(int boostAmount) {
-      this.increaseAttack -= boostAmount;
-      if (this.increaseAttack < 0) {
-         this.increaseAttack = 0;
-      }
-
-      this.attackPower = this.baseAttackPower + this.increaseAttack;
-   }
-
-   public void resetBoosts() {
-      this.increaseAttack = 0;
-      this.attackPower = this.baseAttackPower;
-      this.increaseSpeed = 0;
-      this.speed = this.baseSpeed;
-   }
-
-   public String toString() {
-      return "Name: " + this.name + "\nHP: " + this.hp + "\nAttack Power: " + this.attackPower + "\nMove Type: " + this.moveType + "\nDefender Type: " + this.defenderType + "\nBase Speed: " + this.baseSpeed + "\nBase Attack Power: " + this.baseAttackPower;
-   }
+    public void attack(Pokemon target) {
+        double effectiveness = TypeEffectiveness.getEffectiveness(this.moveType, target.getDefenderType());
+        int damage = (int)((double)this.attackPower * effectiveness);
+        
+        // Apply   the damage to the target's HP
+        target.reduceHp(damage); 
+        
+        System.out.println(this.name + " attacks " + target.getName() + " for " + damage + " damage!");
+        System.out.println(target.getName() + " now has " + target.getHp() + " HP.");
+    }
+   
+    public void applyAttackBoost(int boostAmount) {
+    	this.increaseAttack += boostAmount; // Add to the total boost amount
+        this.attackPower = this.baseAttackPower + this.increaseAttack;
+    }
+    
+    public void applySpeedBoost(int boostAmount) {
+    	this.increaseSpeed += boostAmount; // Add to the total boost amount
+        this.speed = this.baseSpeed + this.increaseSpeed; // Update current speed
+    }
+    
+    public void removeSpeedBoost(int boostAmount) {
+    	this.increaseSpeed -= boostAmount; // Subtract from the total boost amount
+        // Ensure the increase doesn't go negative
+    	if (this.increaseSpeed < 0) {
+    		this.increaseSpeed = 0;
+    	}
+        this.speed = this.baseSpeed + this.increaseSpeed; // Update current speed
+    } 
+    
+    public void removeAttackBoost(int boostAmount) {
+    	this.increaseAttack -= boostAmount; // Subtract from the total boost amount
+        // Ensure the increase doesn't go negative
+    	if (this.increaseAttack < 0) {
+    		this.increaseAttack = 0;
+    	}
+        this.attackPower = this.baseAttackPower + this.increaseAttack; // Update current attackPower
+    }
+    public void resetBoosts() {
+        this.increaseAttack = 0;
+        this.attackPower = this.baseAttackPower; // Reset current attack to base
+        this.increaseSpeed = 0;
+        this.speed = this.baseSpeed;             // Reset current speed to base
+    }
+    			
+    //toString
+    public String toString() {
+        return "Name: " + name +
+               "\nHP: " + hp +
+               "\nAttack Power: " + attackPower +
+               "\nMove Type: " + moveType +
+               "\nDefender Type: " + defenderType +
+               "\nBase Speed: " + baseSpeed +
+               "\nBase Attack Power: " + baseAttackPower;
+}
 }
