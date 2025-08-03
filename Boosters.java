e3ce0068dc62ed56f8464af5b69aa1077b08acc4
wigglywoggly <lolw04938@gmail.com>
2025-08-04 00:17:18 +0800

    Update Boosters(final).java

--- Boosters.java
+++ Boosters.java
@@ -1,28 +1,32 @@
+
 public class Boosters {
-   public static final String Boxing_Gloves = "Boxing Gloves";
-   public static final String Boots = "Boots";
+	    public static final String Boxing_Gloves = "Boxing Gloves";
+	    public static final String Boots = "Boots";
 
-   public static int getAttackBoost(String boosterName) {
-      return "Boxing Gloves".equals(boosterName) ? 10 : 0;
-   }
+	    public static int getAttackBoost(String boosterName) {
+	        if (Boxing_Gloves.equals(boosterName)) {
+	            return 10; // Example: Increase attack by 10
+	        }
+	        return 0;
+	    }
 
-   public static int getSpeedBoost(String boosterName) {
-      return "Boots".equals(boosterName) ? 5 : 0;
-   }
+	    public static int getSpeedBoost(String boosterName) {
+	        if (Boots.equals(boosterName)) {
+	            return 5; // Example: Increase speed by 5 (you might need to add a speed attribute to Pokemon)
+	        }
+	        return 0;
+	    }
 
-   public static String getDescription(String boosterName) {
-      switch(boosterName.hashCode()) {
-      case 64369569:
-         if (boosterName.equals("Boots")) {
-            return "Increases speed during battle.";
-         }
-         break;
-      case 1849161027:
-         if (boosterName.equals("Boxing Gloves")) {
-            return "Increases attack power during battle.";
-         }
-      }
+	    // You might want to add a method to get a description of the booster
+	    public static String getDescription(String boosterName) {
+	        switch (boosterName) {
+	            case Boxing_Gloves:
+	                return "Increases attack power during battle.";
+	            case Boots:
+	                return "Increases speed during battle.";
+	            default:
+	                return "Unknown booster.";
+	        }
+	    }
+	}
 
-      return "Unknown booster.";
-   }
-}
